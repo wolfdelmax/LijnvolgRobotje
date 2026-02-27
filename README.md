0bace4620d29c2856c567307d9df95569ac17291
wolfdelmax <overmeirewolf@gmail.com>
2026-02-27 12:29:56 +0100

    dynamische snelheden voor verschillende rondes en het geheugen voor verschillende rondes

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 https://projecthub.arduino.cc/anova9347/line-follower-robot-with-pid-controller-01813f
 
 prompt : Help me met het schrijven van de code voor een lijnvolgrobot. Dit zijn de componenten : VL6180X ToF distance ranging sensor die een object dat op het circuit ligt moet herkennen. De 8QTR RC gebruiken we om de lijn te zien met de robot
+
+grote aanpassingen om 11:17 zeker kijken naar het toegevoegde bericht
+
+zorgt de constant lezing van ToF sensor voor een vertraging in de lus die niet te verwaarlozen valt. Zorgt deze vertraging voor een vermindering in snelheid  dat de QTR sensoren hun werk kunnen doen? Zorgt deze sensor ervoor dat de energieconsumptie van sensoren te hoog ligt en een impact heeft op de spanning over de motoren? Of maakt het weinig uit aangezien het in de tweede en derde ronde we het kunnnen onthouden op een bepaalde plek?
+
+TO DO:
+< rond het object navigeren en zwarte lijn terug vinden>
+
+
+
+
--- lijnvolg.ino
+++ lijnvolg.ino
@@ -1,10 +1,12 @@
 #include <QTRSensors.h>
 #include <Wire.h>
 #include <Adafruit_VL6180X.h>
+#include <Preferences.h> //slaat de kaart op
 
 // --- COMPONENTEN ---
 QTRSensors qtr;
 Adafruit_VL6180X vl = Adafruit_VL6180X();
+Preferences preferences;
 
 // --- PINNEN ---
 const uint8_t SensorCount = 8;
@@ -12,31 +14,50 @@ uint16_t sensorValues[SensorCount];
 const int motorLinksPWM = 16;  
 const int motorRechtsPWM = 17; 
 
-// --- PD INSTELLINGEN ---
-const int baseSpeed = 150; 
+// --- SNELHEID INSTELLINGEN (PROCENT) ---
+int snelheidMapping = 40;
+int snelheidRace = 85; 
+int baseSpeed;                     
+
+// --- PD INSTELLINGEN --- 
 float Kp = 0.05;     
 float Kd = 0.3;      
 int lastError = 0;   
 
-// --- TOF INSTELLINGEN ---
-const int stopAfstand = 50; 
+// --- TIMING & STATUS ---
+int huidigePoging = 1; 
+bool isFinished = false;
+unsigned long startTime = 0;
+unsigned long finishTimer = 0;
+const int vierkantDetectieTijd = 300; // Voor het 40x40cm zwarte vlak
 
 void setup() {
-  Serial.begin(115200); 
+  Serial.begin(115200);
   Wire.begin();
-
+  
+  // 1. Geheugen laden
+  preferences.begin("robot-data", false);
+  huidigePoging = preferences.getInt("poging", 1);
+  
+  // 2. ToF Initialisatie
   if (!vl.begin()) {
-    Serial.println("ToF sensor niet gevonden!");
+    Serial.println("ToF Fout!");
     while (1); 
   }
 
+  // 3. Snelheid instellen
+  int pct = (huidigePoging == 1) ? snelheidMapping : snelheidRace;
+  baseSpeed = (pct * 255) / 100;
+
   pinMode(motorLinksPWM, OUTPUT);
   pinMode(motorRechtsPWM, OUTPUT);
-
   qtr.setTypeRC(); 
   qtr.setSensorPins((const uint8_t[]){32, 33, 34, 35, 36, 39, 25, 26}, SensorCount);
 
   kalibreerRobot();
+  
+  Serial.print("POGING: "); Serial.println(huidigePoging);
+  startTime = millis();
 }
 
 void loop() {
@@ -60,6 +81,36 @@ void loop() {
 }
 
 // --- FUNCTIES ---
+bool checkStopVak() {
+  int zwartSensoren = 0;
+  for (int i = 0; i < SensorCount; i++) {
+    if (sensorValues[i] > 800) zwartSensoren++;
+  }
+
+  // Als we op een massief zwart vlak rijden (zoals het stopvak van 40cm)
+  if (zwartSensoren >= 7) {
+    if (finishTimer == 0) finishTimer = millis();
+    // Als we al 300ms lang zwart zien, is het GEEN kruispunt maar het stopvak
+    if (millis() - finishTimer > vierkantDetectieTijd) return true;
+  } else {
+    finishTimer = 0; 
+  }
+  return false;
+}
+
+void finishActie() {
+  remmen();
+  isFinished = true;
+  
+  // Volgende poging opslaan voor de herstart
+  int volgende = huidigePoging + 1;
+  if (volgende > 3) volgende = 1; // Na poging 3 resetten naar 1
+  
+  preferences.putInt("poging", volgende);
+  preferences.end();
+  
+  Serial.println("BESTEMMING BEREIKT. Poging opgeslagen in Flash.");
+}
 
 void kalibreerRobot() {
   Serial.println("Kalibratie start...");
