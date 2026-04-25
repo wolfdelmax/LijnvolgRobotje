e47694ab55f998822453f394686ad7146ac6e4df
wolfdelmax <overmeirewolf@gmail.com>
2026-04-25 16:15:57 +0200

    enkel mapping

--- mapping/mapping.ino
+++ mapping/mapping.ino
@@ -1,5 +1,4 @@
 #include <QTRSensors.h>
-#include <Preferences.h>
 
 // ==========================================
 // --- FINETUNING VARIABELEN ---
@@ -10,7 +9,6 @@ int lastError = 0;
 
 // --- SNELHEID INSTELLINGEN (in procenten) ---
 int snelheidMapping    = 40;
-int snelheidRace       = 40;
 int minBochSnelheid    = 25;
 int kalibratieSnelheid = 30;
 
@@ -21,7 +19,7 @@ int eindvlakTicks = 280;
 // --- MAXIMALE DRAAIHOEK (veiligheid tegen doortollen) ---
 // 1 graad = 2.153 ticks per wiel
 const float ticksPerGraad = 2.153f;
-int  maxDraaiGraden = 180;                      // pas dit aan naar smaak
+int  maxDraaiGraden = 180;
 long maxDraaiTicks  = (long)(maxDraaiGraden * ticksPerGraad);
 
 // --- LED ---
@@ -31,7 +29,6 @@ const unsigned long ledKnipperMs = 100;
 // ==========================================
 
 QTRSensors qtr;
-Preferences preferences;
 
 const uint8_t SensorCount = 8;
 uint16_t sensorValues[SensorCount];
@@ -51,25 +48,16 @@ void IRAM_ATTR encoderISR_R() { encoderTellerR++; }
 
 const int pinModeSchakelaar = 1;
 
-int baseSpeed, raceSpeed, minBochSpeed, kalibSpeed;
+int baseSpeed, minBochSpeed, kalibSpeed;
 
-enum RobotModus { MAPPING, WACHT_RACE, RACE, KLAAR };
+enum RobotModus { MAPPING, KLAAR };
 RobotModus huidigeRobotModus = MAPPING;
 
 enum RobotStatus { VOLGEN, NAAR_KRUISPUNT, DRAAIEN, DOORRIJDEN, DOORRIJDEN_UTURN, STOP };
 RobotStatus huidigeStatus = VOLGEN;
 
-const int MAX_PAD_LENGTE = 150;
-char pad[MAX_PAD_LENGTE];
-int  padLengte = 0;
-char verkortPad[MAX_PAD_LENGTE];
-int  verkortLengte = 0;
-int  raceIndex = 0;
-
 unsigned long startTime     = 0;
-unsigned long raceStartTime = 0;
 unsigned long mappingTijdMs = 0;
-unsigned long opgeslagenRaceTijd = 0;
 
 bool snapLinks  = false;
 bool snapRechts = false;
@@ -81,34 +69,15 @@ bool lijnVerlaten = false;
 long draaiStartTicksL = 0;
 long draaiStartTicksR = 0;
 
-bool vorigeSchakelaarHoog = true;
-
 // LED knipperstatus
 unsigned long ledLaatsteWissel = 0;
 bool ledStatus = false;
 
-const int MAX_EVENTS = 200;
-struct EventEntry {
-  unsigned long tijd;
-  char  event;
-  int   sensorLinks;
-  int   sensorRechts;
-  int   extra;
-};
-EventEntry mappingLog[MAX_EVENTS];
-int        mappingLogIdx = 0;
-EventEntry raceLog[MAX_EVENTS];
-int        raceLogIdx = 0;
-
 // --- FORWARD DECLARATIES ---
-void optimaliseerPad(char* padArray, int &lengte);
 void kalibreerRobot();
 void runMapping();
-void runRace();
 void finishMapping();
-void finishRace();
-void resetVoorRace();
-void updateLedRace();
+void updateLed();
 bool draaihoekOverschreden();
 
 void setup() {
@@ -117,7 +86,6 @@ void setup() {
   digitalWrite(pinLed, LOW);
 
   baseSpeed    = (snelheidMapping    * 255) / 100;
-  raceSpeed    = (snelheidRace       * 255) / 100;
   minBochSpeed = (minBochSnelheid    * 255) / 100;
   kalibSpeed   = (kalibratieSnelheid * 255) / 100;
 
@@ -137,42 +105,9 @@ void setup() {
   qtr.setTypeRC();
   qtr.setSensorPins(sensorPinnen, SensorCount);
 
-  preferences.begin("robot-data", false);
-  int runState = preferences.getInt("state", 0);
+  huidigeRobotModus = MAPPING;
+  kalibreerRobot();
 
-  if (runState == 0) {
-    huidigeRobotModus = MAPPING;
-    kalibreerRobot();
-  } 
-  else if (runState == 1) {
-    padLengte = preferences.getInt("padLen", 0);
-    preferences.getBytes("pad", pad, MAX_PAD_LENGTE);
-    verkortLengte = preferences.getInt("vpadLen", 0);
-    preferences.getBytes("vpad", verkortPad, MAX_PAD_LENGTE);
-    mappingTijdMs = preferences.getULong("mTijd", 0);
-    mappingLogIdx = preferences.getInt("mLogCnt", 0);
-    preferences.getBytes("mLog", mappingLog, sizeof(mappingLog));
-
-    huidigeRobotModus = WACHT_RACE;
-    kalibreerRobot();
-  }
-  else if (runState == 2) {
-    padLengte = preferences.getInt("padLen", 0);
-    preferences.getBytes("pad", pad, MAX_PAD_LENGTE);
-    verkortLengte = preferences.getInt("vpadLen", 0);
-    preferences.getBytes("vpad", verkortPad, MAX_PAD_LENGTE);
-    mappingTijdMs = preferences.getULong("mTijd", 0);
-    mappingLogIdx = preferences.getInt("mLogCnt", 0);
-    preferences.getBytes("mLog", mappingLog, sizeof(mappingLog));
-
-    opgeslagenRaceTijd = preferences.getULong("rTijd", 0);
-    raceLogIdx = preferences.getInt("rLogCnt", 0);
-    preferences.getBytes("rLog", raceLog, sizeof(raceLog));
-
-    huidigeRobotModus = KLAAR;
-  }
-  
-  preferences.end();
   startTime = millis();
 }
 
@@ -182,29 +117,8 @@ void loop() {
   if (huidigeRobotModus == KLAAR) {
     remmen();
     digitalWrite(pinLed, LOW);
-
-    if (vorigeSchakelaarHoog && !schakelaarHoog) {
-      preferences.begin("robot-data", false);
-      preferences.clear();
-      preferences.end();
-      huidigeRobotModus = (RobotModus)99;
-    }
-    vorigeSchakelaarHoog = schakelaarHoog;
     return;
   }
-
-  if (huidigeRobotModus == WACHT_RACE) {
-    remmen();
-    digitalWrite(pinLed, LOW);
-    if (vorigeSchakelaarHoog && !schakelaarHoog) {
-      resetVoorRace();
-      delay(200);
-    }
-    vorigeSchakelaarHoog = schakelaarHoog;
-    return;
-  }
-
-  vorigeSchakelaarHoog = schakelaarHoog;
 
   if (schakelaarHoog) {
     remmen();
@@ -213,38 +127,15 @@ void loop() {
   }
 
   if (huidigeRobotModus == MAPPING) {
-    digitalWrite(pinLed, LOW);
     runMapping();
-  }
-  else if (huidigeRobotModus == RACE) {
-    runRace();
-    updateLedRace();
+    updateLed();
   }
 }
 
-void resetVoorRace() {
-  huidigeRobotModus = RACE;
-  huidigeStatus     = VOLGEN;
-  raceIndex         = 0;
-  raceLogIdx        = 0;
-  lastError         = 0;
-  encoderTellerL    = 0;
-  encoderTellerR    = 0;
-  snapLinks         = false;
-  snapRechts        = false;
-  lijnVerlaten      = false;
-  actieStartTijd    = 0;
-  raceStartTime     = millis();
-  ledLaatsteWissel  = 0;
-  ledStatus         = false;
-
-  qtr.readLineBlack(sensorValues);
-}
-
 // ==========================================
-// LED STURING (alleen in race)
+// LED STURING
 // ==========================================
-void updateLedRace() {
+void updateLed() {
   if (huidigeStatus == VOLGEN) {
     digitalWrite(pinLed, HIGH);
     ledStatus = true;
@@ -269,9 +160,6 @@ void updateLedRace() {
 // ==========================================
 // DRAAIHOEK BEWAKING
 // ==========================================
-// Encoders tellen altijd op (RISING-only). Bij een draai op de plaats
-// draaien beide wielen ongeveer evenveel, dus gemiddelde van de delta
-// is een goede maat voor de draaihoek in ticks.
 bool draaihoekOverschreden() {
   long deltaL = encoderTellerL - draaiStartTicksL;
   long deltaR = encoderTellerR - draaiStartTicksR;
@@ -320,7 +208,6 @@ void runMapping() {
 
       if (zwartTel >= 6) {
         if (gemTicks >= eindvlakTicks) {
-          logMapping('F', 0);
           finishMapping();
         } else {
           setMotorLinks(baseSpeed);
@@ -337,9 +224,9 @@ void runMapping() {
 
       bool kanS = (sensorValues[3] > 600 || sensorValues[4] > 600);
 
-      if (snapLinks)       { pad[padLengte++] = 'L'; logMapping('L', 0); startDraai(-baseSpeed, baseSpeed); }
-      else if (kanS)       { pad[padLengte++] = 'S'; logMapping('S', 0); huidigeStatus = VOLGEN; }
-      else if (snapRechts) { pad[padLengte++] = 'R'; logMapping('R', 0); startDraai(baseSpeed, -baseSpeed); }
+      if (snapLinks)       { startDraai(-baseSpeed, baseSpeed); }
+      else if (kanS)       { huidigeStatus = VOLGEN; }
+      else if (snapRechts) { startDraai(baseSpeed, -baseSpeed); }
       else                 { startUTurnMapping(); }
       break;
     }
@@ -354,7 +241,6 @@ void runMapping() {
       }
       // Veiligheid: maximale draaihoek overschreden -> afbreken
       if (draaihoekOverschreden()) {
-        logMapping('X', maxDraaiGraden);
         huidigeStatus = VOLGEN;
         break;
       }
@@ -379,150 +265,13 @@ void runMapping() {
 }
 
 // ==========================================
-// RACE STATE MACHINE
+// FINISH FUNCTIE
 // ==========================================
-void runRace() {
-  uint16_t positie = qtr.readLineBlack(sensorValues);
-
-  switch (huidigeStatus) {
-
-    case VOLGEN: {
-      bool bL = (sensorValues[0] > 600 || sensorValues[1] > 600);
-      bool bR = (sensorValues[6] > 600 || sensorValues[7] > 600);
-
-      if (bL || bR) {
-        snapLinks = bL; snapRechts = bR;
-        startNaarKruispunt(raceSpeed);
-        break;
-      }
-      if (isDoodlopend()) {
-        logRace('!', raceIndex);
-        encoderTellerL = encoderTellerR = 0;
-        huidigeStatus = DOORRIJDEN_UTURN;
-        break;
-      }
-
-      int error = (int)positie - 3500;
-      float dyn = constrain(raceSpeed - abs(error) * 0.025f, (float)minBochSpeed, (float)raceSpeed);
-      int cor = (int)(error * Kp + (error - lastError) * Kd);
-      lastError = error;
-      rijden((int)dyn, cor);
-      break;
-    }
-
-    case NAAR_KRUISPUNT: {
-      int zwartTel = 0;
-      for (int i = 0; i < SensorCount; i++) if (sensorValues[i] > 600) zwartTel++;
-      long gemTicks = (encoderTellerL + encoderTellerR) / 2;
-
-      if (zwartTel >= 6) {
-        if (gemTicks >= eindvlakTicks) {
-          logRace('F', raceIndex);
-          finishRace();
-        } else {
-          setMotorLinks(raceSpeed);
-          setMotorRechts(raceSpeed);
-        }
-        break;
-      }
-
-      if (gemTicks < doorrijTicks) {
-        setMotorLinks(raceSpeed);
-        setMotorRechts(raceSpeed);
-        break;
-      }
-
-      if (raceIndex < verkortLengte) {
-        char inst = verkortPad[raceIndex++];
-        logRace(inst, raceIndex - 1);
-
-        if      (inst == 'L') startDraai(-raceSpeed,  raceSpeed);
-        else if (inst == 'R') startDraai( raceSpeed, -raceSpeed);
-        else if (inst == 'S') huidigeStatus = VOLGEN;
-        else if (inst == 'U') startDraai( raceSpeed, -raceSpeed);
-      } else {
-        logRace('?', raceIndex);
-        huidigeStatus = VOLGEN;
-      }
-      break;
-    }
-
-    case DRAAIEN:
-      qtr.readLineBlack(sensorValues);
-      if (!lijnVerlaten) {
-        if (sensorValues[3] < 300 && sensorValues[4] < 300) lijnVerlaten = true;
-      } else if (sensorValues[3] > 500 || sensorValues[4] > 500) {
-        huidigeStatus = VOLGEN;
-        break;
-      }
-      // Veiligheid: maximale draaihoek overschreden -> afbreken
-      if (draaihoekOverschreden()) {
-        logRace('X', maxDraaiGraden);
-        huidigeStatus = VOLGEN;
-        break;
-      }
-      if (millis() - actieStartTijd > 1500) huidigeStatus = VOLGEN;
-      break;
-
-    case DOORRIJDEN:
-      if ((encoderTellerL + encoderTellerR) / 2 >= doorrijTicks) huidigeStatus = VOLGEN;
-      break;
-
-    case DOORRIJDEN_UTURN:
-      setMotorLinks(raceSpeed);
-      setMotorRechts(raceSpeed);
-      if ((encoderTellerL + encoderTellerR) / 2 >= doorrijTicks) {
-        startDraai(raceSpeed, -raceSpeed);
-      }
-      break;
-
-    case STOP:
-      remmen();
-      break;
-  }
-}
-
-// ==========================================
-// FINISH FUNCTIES
-// ==========================================
-
 void finishMapping() {
   remmen();
   mappingTijdMs = millis() - startTime;
-
-  memcpy(verkortPad, pad, padLengte);
-  verkortLengte = padLengte;
-  optimaliseerPad(verkortPad, verkortLengte);
-
-  preferences.begin("robot-data", false);
-  preferences.putInt("state", 1);
-  preferences.putInt("padLen", padLengte);
-  preferences.putBytes("pad", pad, MAX_PAD_LENGTE);
-  preferences.putInt("vpadLen", verkortLengte);
-  preferences.putBytes("vpad", verkortPad, MAX_PAD_LENGTE);
-  preferences.putULong("mTijd", mappingTijdMs);
-  preferences.putInt("mLogCnt", mappingLogIdx);
-  preferences.putBytes("mLog", mappingLog, sizeof(mappingLog));
-  preferences.end();
-
-  huidigeRobotModus = WACHT_RACE;
-  vorigeSchakelaarHoog = false;
-}
-
-void finishRace() {
-  remmen();
-  unsigned long raceTijdMs = millis() - raceStartTime;
-  opgeslagenRaceTijd = raceTijdMs;
-
-  preferences.begin("robot-data", false);
-  preferences.putInt("state", 2);
-  preferences.putULong("rTijd", raceTijdMs);
-  preferences.putInt("rLogCnt", raceLogIdx);
-  preferences.putBytes("rLog", raceLog, sizeof(raceLog));
-  preferences.end();
-
+  huidigeStatus = STOP;
   huidigeRobotModus = KLAAR;
-  vorigeSchakelaarHoog = false;
 }
 
 // ==========================================
@@ -554,10 +303,6 @@ void startDraai(int spdL, int spdR) {
 }
 
 void startUTurnMapping() {
-  if (padLengte < MAX_PAD_LENGTE) {
-    pad[padLengte++] = 'U';
-    logMapping('U', 0);
-  }
   startDraai(baseSpeed, -baseSpeed);
 }
 
@@ -585,22 +330,6 @@ void remmen() {
   setMotorRechts(0);
 }
 
-void logMapping(char event, int extra) {
-  if (mappingLogIdx < MAX_EVENTS) {
-    mappingLog[mappingLogIdx] = { millis() - startTime, event,
-                                  (int)sensorValues[0], (int)sensorValues[7], extra };
-    mappingLogIdx++;
-  }
-}
-
-void logRace(char event, int padIdx) {
-  if (raceLogIdx < MAX_EVENTS) {
-    raceLog[raceLogIdx] = { millis() - raceStartTime, event,
-                            (int)sensorValues[0], (int)sensorValues[7], padIdx };
-    raceLogIdx++;
-  }
-}
-
 void kalibreerRobot() {
   pinMode(pinLed, OUTPUT);
   digitalWrite(pinLed, HIGH);
@@ -614,33 +343,4 @@ void kalibreerRobot() {
   remmen();
   digitalWrite(pinLed, LOW);
   delay(1000);
-}
-
-void optimaliseerPad(char* padArray, int &lengte) {
-  bool veranderd = true;
-  while (veranderd) {
-    veranderd = false;
-    for (int i = 0; i < lengte - 2; i++) {
-      if (padArray[i + 1] == 'U') {
-        char actie = ' ';
-        if      (padArray[i] == 'L' && padArray[i+2] == 'R') actie = 'U';
-        else if (padArray[i] == 'L' && padArray[i+2] == 'S') actie = 'R';
-        else if (padArray[i] == 'R' && padArray[i+2] == 'L') actie = 'U';
-        else if (padArray[i] == 'S' && padArray[i+2] == 'L') actie = 'R';
-        else if (padArray[i] == 'S' && padArray[i+2] == 'S') actie = 'U';
-        else if (padArray[i] == 'L' && padArray[i+2] == 'L') actie = 'S';
-        else if (padArray[i] == 'R' && padArray[i+2] == 'R') actie = 'S';
-        else if (padArray[i] == 'R' && padArray[i+2] == 'S') actie = 'L';
-        else if (padArray[i] == 'S' && padArray[i+2] == 'R') actie = 'L';
-
-        if (actie != ' ') {
-          padArray[i] = actie;
-          for (int j = i + 1; j < lengte - 2; j++) padArray[j] = padArray[j + 2];
-          lengte -= 2;
-          veranderd = true;
-          break;
-        }
-      }
-    }
-  }
 }
